93cbec97274974a329b27310b57cdce06d9949f7
Hektor-Byfors <hektor.byfors@hotmail.com>
2022-06-03 17:04:29 +0200

    added users for frontend GET

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const User = require("./models/user.js");
+const cors = require("cors");
 
 const app = express();
 app.set("view engine", "ejs");
-
+app.use(cors());
 const port = 3001;
 const dbURI = "mongodb+srv://nodeDB:Nlfr7lsygbMhdcBb@nodedb.0wfhner.mongodb.net/nodeNewsLetter?retryWrites=true&w=majority"
 
@@ -34,13 +35,24 @@ app.get("/add-user", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-    //res.render("index");
     User.find()
     .then((result) => {
         res.render("index", { result });
     })
     .catch((err) => {
         console.log(err);
-        res.send("error has occurred" + err)
+        res.send("error has occurred:" + err);
     })
 });
+
+app.get("/users", (req, res) => {
+    
+    User.find()
+    .then((result) => {
+        res.send(result);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.send("error has occurred:" + err);
+    })
+})
